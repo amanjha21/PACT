d35a99e1a07c66b3c288ec0e58d60f585ab0fc70
Aman Jha <aman.jha21.aj@gmail.com>
2022-04-23 15:31:08 +0545

    update link

--- backend/helpers/passportSetup.js
+++ backend/helpers/passportSetup.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "https://pcon-kalyan.herokuapp.com/auth/google/callback",
     },
     function async(accessToken, refreshToken, profile, cb) {
       return cb(null, profile);
